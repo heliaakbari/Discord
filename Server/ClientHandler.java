ba52c968ff023331ad37f279ec408cc5534aeee2
heliaakbari <heliaakbari@aut.ac.ir>
2022-06-27 16:19:04 +0430

    changes in server

--- Server/ClientHandler.java
+++ Server/ClientHandler.java
@@ -1,3 +1,7 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.net.SocketException;
 
@@ -5,15 +9,34 @@ public class ClientHandler extends Thread{
     private Socket client;
     private ServerSide serverSide;
     private String threadID;
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
 
-    public ClientHandler(Socket client, ServerSide serverSide) throws SocketException {
+    public ClientHandler(Socket client, ObjectInputStream in, ObjectOutputStream out, ServerSide serverSide) throws SocketException {
         this.client = client;
+        this.in = in;
+        this.out = out;
         this.serverSide = serverSide;
     }
 
     @Override
     public void run() {
+        Listener listener = new Listener(this,in);
+        listener.start();
+    }
 
+    public Data getCommandFromListener(Command cmd){
+        Data dt = serverSide.moveCmd(cmd,this);
+        try {
+            out.writeObject(dt);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return dt;
+    }
+
+    public void sendInstantMessage(Data dt){
 
     }
+
 }
--- Server/ServerSide.java
+++ Server/ServerSide.java
@@ -5,26 +5,32 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ServerSide {
     //private Inventory inventory;
     private ServerSocket serverSocket;
-    private ArrayList<ClientHandler> clientHandlers;
+    private HashMap<String,ClientHandler> clientHandlers;
     private Socket client;
     private ObjectOutputStream out;
     private ObjectInputStream in;
-
+    private DatabaseManager dbm = new DatabaseManager();
+    //first one is the person and the other is their friend
+    private HashMap<String,String> activePvs= new HashMap<>();
+    //first one is the user and the other is server/channel form
+    private HashMap<String,ArrayList<String>> activeChannels= new HashMap<>();
 
     public ServerSide() {
         try {
             serverSocket = new ServerSocket(8642);
-            clientHandlers = new ArrayList<>();
+            clientHandlers = new HashMap<>();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void startServer(){
+    public void startServer() {
+        dbm.start();
 
         while (true) {
 
@@ -33,7 +39,7 @@ public class ServerSide {
             try {
                 client = serverSocket.accept();
                 out = new ObjectOutputStream(client.getOutputStream());
-                in  = new ObjectInputStream(client.getInputStream());
+                in = new ObjectInputStream(client.getInputStream());
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -42,8 +48,7 @@ public class ServerSide {
             // creating new thread for handling new connection
             ClientHandler clientHandler;
             try {
-                clientHandler = new ClientHandler(client, this);
-                clientHandlers.add(clientHandler);
+                clientHandler = new ClientHandler(client, in, out, this);
                 Thread thread = new Thread(clientHandler);
                 thread.start();
             } catch (SocketException e) {
@@ -52,5 +57,42 @@ public class ServerSide {
         }
     }
 
+    public void addClientHandler (String username,ClientHandler clientHandler ){
+        clientHandlers.put(username,clientHandler);
+    }
 
+    public Data moveCmd(Command cmd,ClientHandler clientHandler){
+        Data dt = dbm.cmdManager.process(cmd);
+        return dt;
+    }
+
+    public void instantPvMsg(Command cmd){
+        if(cmd.getKeyword().equals("newPvMsg")){
+            String receiver = (String) cmd.getSecondary();
+            if(activePvs.containsKey(receiver)){
+               String sender = activePvs.get(receiver);
+               if(sender.equals(cmd.getUser())){
+                   Data dt = Data.newPvMsg(receiver,(Message) cmd.getPrimary());
+                   clientHandlers.get(receiver).sendInstantMessage(dt);
+               }
+            }
+        }
+    }
+
+    public void instantChannelMsg(Command cmd){
+        if(cmd.getKeyword().equals("newChannelMsg")) {
+           String place = cmd.getServer()+"/"+cmd.getChannel();
+           ArrayList<String> onlinePeople = new ArrayList<>();
+            for (HashMap.Entry<String, ArrayList<String>> set :
+                    activeChannels.entrySet()) {
+                if(set.getValue().get(0).equals(cmd.getServer()) && set.getValue().get(1).equals(cmd.getChannel() )){
+                    onlinePeople.add(set.getKey());
+                }
+            }
+            Data dt = Data.newChannelMsg(cmd.getServer(),cmd.getChannel(),(Message)cmd.getPrimary());
+            for(String person : onlinePeople){
+                clientHandlers.get(person).sendInstantMessage(dt);
+            }
+        }
+    }
 }
